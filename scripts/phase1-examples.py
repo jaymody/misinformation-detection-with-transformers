ca16106e91faec82f3f0e48a6260d5c62e27fc3c
jaymody <jaykmody@gmail.com>
2020-04-26 20:02:46 -0400

    update scripts for new workflow

---
+++ scripts/phase1-examples.py
@@ -0,0 +1,111 @@
+import os
+import json
+import glob
+import argparse
+
+from tqdm import tqdm
+
+from valerie.utils import get_logger
+from valerie.data import Claim, Article
+from valerie.processors import MultiClaimSupportProcessor
+
+_logger = get_logger()
+
+
+def load_claims(claims_file):
+    _logger.info("... loading claims from %s ...", claims_file)
+    with open(claims_file, 'r') as fi:
+        claims = [Claim.from_dict(d) for d in tqdm(json.load(fi))]
+    return claims
+
+
+def load_articles(articles_dir):
+    _logger.info("... loading articles from %s ...", articles_dir)
+    articles = {}
+    for filepath in tqdm(glob.glob(os.path.join(articles_dir, "*.txt"))):
+        with open(filepath) as fi:
+            article_id = int(os.path.splitext(os.path.basename(filepath))[0])
+            articles[article_id] = Article.from_txt(article_id, fi.read())
+    return articles
+
+
+def load_processor(articles, word2vec_file, keep_n, min_threshold, min_examples, nproc):
+    _logger.info("... loading processor ...")
+    processor = MultiClaimSupportProcessor(
+        articles=articles,
+        word2vec_file=word2vec_file,
+        keep_n=keep_n,
+        min_threshold=min_threshold,
+        min_examples=min_examples,
+        nproc=nproc
+    )
+    return processor
+
+
+def generate_examples(claims, processor):
+    _logger.info("... generating examples ...")
+    examples = processor.generate_examples(claims)
+    return examples
+
+
+def generate_train_test_examples(claims, processor, train_test_split_ratio=0.9):
+    split_num = int(len(claims) * train_test_split_ratio)
+    train_claims, test_claims = claims[:split_num], claims[split_num:]
+
+    _logger.info("train_test_split_ratio = %f ...", train_test_split_ratio)
+    _logger.info("      num_total_claims = %d ...", len(claims))
+    _logger.info("   num_training_claims = %d ...", len(train_claims))
+    _logger.info("    num_testing_claims = %d ...", len(test_claims))
+
+    _logger.info("... generating train examples ...")
+    train_examples = processor.generate_examples(train_claims)
+
+    _logger.info("... generating test examples ...")
+    test_examples = processor.generate_examples(test_claims)
+
+    _logger.info("    num_total_examples = %d ...", len(train_examples+test_examples))
+    _logger.info(" num_training_examples = %d ...", len(train_examples))
+    _logger.info("  num_testing_examples = %d ...", len(test_examples))
+
+    return train_examples, test_examples
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Generate phase1 examples.")
+    parser.add_argument("--claims_file", type=str)
+    parser.add_argument("--articles_dir", type=str)
+    parser.add_argument("--word2vec_file", type=str)
+    parser.add_argument("--examples_file", type=str)
+    parser.add_argument("--keep_n", type=int)
+    parser.add_argument("--min_threshold", type=float)
+    parser.add_argument("--min_examples", type=int)
+    parser.add_argument("--nproc", type=int)
+    parser.add_argument("--train_test_split_ratio", type=float, default=None)
+
+    args = parser.parse_args()
+    claims = load_claims(args.claims_file)
+    articles = load_claims(args.articles_dir)
+    processor = load_processor(
+        articles,
+        args.word2vec_file,
+        args.keep_n,
+        args.min_threshold,
+        args.min_examples,
+        args.nproc
+    )
+
+    if args.train_test_split_ratio is None:
+        examples = generate_examples(claims, processor)
+        _logger.info("... saving examples to %s ...", args.examples_file)
+        with open(args.examples_file, 'w') as fo:
+            json.dump([example.__dict__ for example in examples], fo)
+    else:
+        train_examples, test_examples = generate_train_test_examples(claims, processor, args.train_test_split_ratio)
+        _logger.info("... saving examples to %s ...", args.examples_file)
+        with open(args.examples_file, 'w') as fo:
+            json.dump({
+                "training_examples": [example.__dict__ for example in train_examples],
+                "testing_examples": [example.__dict__ for example in test_examples],
+            }, fo)
+
+
--- scripts/phase1-predict.py
+++ scripts/phase1-predict.py
@@ -1,44 +1,59 @@
-import os
 import json
-import glob
 import argparse
 import collections
-import multiprocessing
 
 import numpy as np
-from tqdm import tqdm
+from transformers import (
+    AutoConfig,
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    Trainer,
+    TrainingArguments,
+    InputExample,
+)
 
 from valerie.utils import get_logger
-from valerie.modeling import predict
+from valerie.datasets import BasicDataset
 
 _logger = get_logger()
 
 
-def generate_predictions(examples_file, predictions_file, pretrained_model_name_or_path, use_cuda, cuda_device, batch_size, ngpu):
-    _logger.info("... loading examples from {} ...".format(examples_file))
-    with open(examples_file, 'r') as fi:
-        examples = json.load(fi)
+def load_examples(examples_file):
+    _logger.info("... loading examples from %s ...", examples_file)
+    with open(examples_file) as fi:
+        examples = [InputExample(**example) for example in json.load(fi)]
+    return examples
 
-    _logger.info("... computing predictions ...")
-    output = predict(
-        examples,
-        pretrained_model_name_or_path,
-        use_cuda=use_cuda,
-        cuda_device=cuda_device,
-        batch_size=batch_size
+
+def generate_predictions(examples, pretrained_model_name_or_path, batch_size, ngpu, nproc):
+    _logger.info("... loading config, tokenizer, and model from %s ...", pretrained_model_name_or_path)
+    config = AutoConfig.from_pretrained(pretrained_model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
+    model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_name_or_path, config=config)
+
+    _logger.info("... loading dataset ...")
+    predict_dataset = BasicDataset(examples, tokenizer, [0,1,2], nproc=nproc)
+    predict_args = TrainingArguments(
+        eval_batch_size=batch_size,
+        n_gpu=ngpu,
+        do_predict=True,
     )
 
+    _logger.info("... loading trainer ...")
+    trainer = Trainer(model=model, args=predict_args)
+
+    _logger.info("... predicting ...")
+    output = trainer.predict(predict_dataset)
+
     _logger.info("... averaging predictions across claim_id ...")
+    assert len(examples) == len(output.predictions)
     probs = collections.defaultdict(list)
-    for example, prob in zip(examples, output[1]):
+    for example, prob in zip(examples, output.predictions):
         probs[example.guid].append(prob)
     averaged_probs = {k: np.mean(v, axis=0) for k,v in probs.items()}
     predictions = {k: np.argmax(v) for k,v in averaged_probs.items()}
 
-    _logger.info("... saving predictions to {} ...".format(predictions_file))
-    with open(predictions_file, 'w') as fo:
-        for claim_id, pred in predictions.items():
-            fo.write("%d,%d\n" % (claim_id, pred))
+    return predictions
 
 
 if __name__ == "__main__":
@@ -46,11 +61,21 @@ if __name__ == "__main__":
     parser.add_argument("--examples_file", type=str)
     parser.add_argument("--predictions_file", type=str)
     parser.add_argument("--pretrained_model_name_or_path", type=str)
-    parser.add_argument("--use_cuda", type=bool)
-    parser.add_argument("--cuda_device", type=int)
     parser.add_argument("--batch_size", type=int)
     parser.add_argument("--ngpu", type=int)
 
-    kwargs = parser.parse_args()
-    generate_predictions(**kwargs.__dict__)
+    args = parser.parse_args()
+    examples = load_examples(args.examples_file)
+    predictions = generate_predictions(
+        examples,
+        args.pretrained_model_name_or_path,
+        args.batch_size,
+        args.ngpu,
+        args.nproc
+    )
+
+    _logger.info("... saving predictions to %s ...", args.predictions_file)
+    with open(args.predictions_file, 'w') as fo:
+        for claim_id, pred in predictions.items():
+            fo.write("%d,%d\n" % (claim_id, pred))
 
