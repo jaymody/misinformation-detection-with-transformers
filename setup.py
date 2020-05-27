2618f9d0c33631c5400c8269d86dd4ecd2fb5f6b
jaymody <jaykmody@gmail.com>
2020-05-27 02:27:27 -0400

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
-with open('README.md', 'r') as fi:
+with open("README.md", "r") as fi:
     long_description = fi.read()
 
 setuptools.setup(
-	name='valerie',
-	version='0.1',
-	author='Jay Mody',
-	author_email='jaykmody@gmail.com',
-	description='Fake news detection.',
-	long_description = long_description,
-	long_description_content_type = 'text/markdown',
-	url='https://github.com/jaymody/valerie',
-	packages=setuptools.find_packages(),
-	install_requires=[],
-	classifiers=[]
+    name="valerie",
+    version="0.2",
+    author="Jay Mody",
+    author_email="jaykmody@gmail.com",
+    description="Fake news detection.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaymody/valerie",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[],
 )
