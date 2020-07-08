541fa7fad7cc944d0e551393978a9f672cd84773
jaymody <jaykmody@gmail.com>
2020-07-08 00:16:46 -0400

    added sensible options to score_predictions_file.py

--- scripts/score_predictions_file.py
+++ scripts/score_predictions_file.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--metadata_file", type=str)
     parser.add_argument("--predictions_file", type=str)
-    parser.add_argument("--score_file", type=str)
-    parser.add_argument("--report_file", type=str)
+    parser.add_argument("--score_file", type=str, default=None)
+    parser.add_argument("--report_file", type=str, default=None)
+    parser.add_argument("--print", type=bool, default=True)
     args = parser.parse_args()
 
     with open(args.metadata_file) as fi:
@@ -21,7 +22,16 @@ if __name__ == "__main__":
         raise ValueError("validate_predictions_phase2 failed")
     report, output, official_output = compute_detailed_score_phase2(labels, predictions)
 
-    with open(args.report_file, "w") as fo:
-        fo.write(report)
-    with open(args.score_file, "w") as fo:
-        json.dump(output, fo, indent=2)
+    if args.print:
+        print("score:")
+        print(json.dumps(output, indent=2))
+        print()
+        print("report:")
+        print(report)
+
+    if args.report_file:
+        with open(args.report_file, "w") as fo:
+            fo.write(report)
+    if args.score_file:
+        with open(args.score_file, "w") as fo:
+            json.dump(output, fo, indent=2)
