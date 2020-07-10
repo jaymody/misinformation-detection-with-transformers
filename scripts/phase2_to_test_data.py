8af09d3521a79fa89dff19233edc7e7f58488db1
jaymody <jaykmody@gmail.com>
2020-07-09 20:54:00 -0400

    updated test data creation script to be more accurate

--- scripts/phase2_to_test_data.py
+++ scripts/phase2_to_test_data.py
@@ -1,26 +1,35 @@
 import os
+import copy
 import json
 import argparse
 from pathlib import Path
 
 
-def phase2_to_test_data(metadata_file, output_dir, truncate):
+def phase2_to_test_data(metadata_file, output_dir, truncate=None):
     with open(metadata_file) as fi:
         metadata = json.load(fi)
+    metadata = metadata[:truncate]
 
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
 
-    metadata = metadata[:truncate]
+    # labelled
+    metadata_labelled = {str(claim["id"]): claim for claim in copy.deepcopy(metadata)}
+    for claim in metadata_labelled.values():
+        del claim["claim"]
+        del claim["claimant"]
+        del claim["date"]
+        del claim["id"]
     with open(os.path.join(output_dir, "metadata-labelled.json"), "w") as fo:
-        json.dump(metadata, fo, indent=2)
+        json.dump(metadata_labelled, fo, indent=2)
 
-    for claim in metadata:
+    # unlabelled
+    metadata_unlabelled = copy.deepcopy(metadata)
+    for claim in metadata_unlabelled:
         del claim["label"]
         del claim["related_articles"]
-
     with open(os.path.join(output_dir, "metadata-unlabelled.json"), "w") as fo:
-        json.dump(metadata, fo, indent=2)
+        json.dump(metadata_unlabelled, fo, indent=2)
 
 
 if __name__ == "__main__":
